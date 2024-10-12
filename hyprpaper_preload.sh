dc9a3f8a9e8e3ea7b33ddaf78364215adbcec0ec
Indigo Hartsell <indiharts@proton.me>
2024-10-12 09:07:01 -0400

    updated hyprpaper_preload to reset hyprpaper on run

--- hyprpaper_preload.sh
+++ hyprpaper_preload.sh
@@ -2,14 +2,19 @@
 
 preload_wallpaper() {
 
+    # config
     monitor=""
     wallpaper_dir="$HOME/Pictures/bg"
     hp_conf="$HOME/.config/hypr/hyprpaper.conf"
 
-    # build hyprpaper.conf
+    # var
+    NOW=`date '+%F_%H:%M:%S'`
     output_string=""
+
+    # build hyprpaper.conf
     output_string+="# --- hyprpaper autogen by indigo hartsell ---\n"
     output_string+="# random wallpaper from $wallpaper_dir\n"
+    output_string+="# ran $NOW\n"
     output_string+="\n"
 
     wallpapers=$(find "$wallpaper_dir" -type f | sort -R --random-source=/dev/urandom)
@@ -25,4 +30,24 @@ preload_wallpaper() {
 
 }
 
+reset_hyprpaper() {
+    # Check if hyprpaper is running
+    if pgrep -x "hyprpaper" > /dev/null; then
+        killall hyprpaper
+        
+        # Wait a moment to ensure it has terminated
+        sleep 0.1
+    else
+        echo "Hyprpaper is not running."
+    fi
+
+    # Start a new instance of hyprpaper
+    hyprpaper &
+
+    # Notify success
+    echo "Hyprpaper has been restarted."
+}
+
 preload_wallpaper
+
+reset_hyprpaper

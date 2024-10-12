0308f117eb5375833039303142b85716953bfa08
Indigo Hartsell <indiharts@proton.me>
2024-10-11 22:27:58 -0400

    refactor hyprpaper preload

--- hyprpaper_preload.sh
+++ hyprpaper_preload.sh
@@ -3,17 +3,25 @@
 preload_wallpaper() {
 
     monitor=""
-
     wallpaper_dir="$HOME/Pictures/bg"
+    hp_conf="$HOME/.config/hypr/hyprpaper.conf"
+
+    # build hyprpaper.conf
+    output_string=""
+    output_string+="# --- hyprpaper autogen by indigo hartsell ---\n"
+    output_string+="# random wallpaper from $wallpaper_dir\n"
+    output_string+="\n"
 
     wallpapers=$(find "$wallpaper_dir" -type f | sort -R --random-source=/dev/urandom)
-
     for wallpaper in $wallpapers; do
         preload_string="preload = "$wallpaper"\n"
         wallpaper_string="wallpaper = $monitor, $wallpaper\n"
     done
 
-    echo -en "$preload_string$wallpaper_string" > ~/.config/hypr/hyprpaper.conf
+    output_string+="$preload_string"
+    output_string+="$wallpaper_string"
+
+    echo -en "$output_string" > $hp_conf
 
 }
 

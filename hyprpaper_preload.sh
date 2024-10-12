0fbce920df95050ab8132b8192ea877392d94cb1
Indigo Hartsell <indiharts@proton.me>
2024-10-11 22:12:39 -0400

    chore

---
+++ hyprpaper_preload.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+preload_wallpaper() {
+
+    monitor=""
+
+    wallpaper_dir="$HOME/Pictures/bg"
+
+    wallpapers=$(find "$wallpaper_dir" -type f | sort -R )
+
+    for wallpaper in $wallpapers; do
+        preload_string+="preload = "$wallpaper"\n"
+        wallpaper_string+="wallpaper = $monitor, $wallpaper\n"
+    done
+
+    echo -en "$preload_string$wallpaper_string" > ~/.config/hypr/hyprpaper.conf
+
+}
+
+preload_wallpaper
